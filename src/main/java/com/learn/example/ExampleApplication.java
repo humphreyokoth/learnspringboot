0f61c6225e8fd8ec5cc0f212f2e904f8b0dfe544
humphreyokoth <humpasp@gmail.com>
2025-02-14 12:09:13 +0300

    feat:reading properties

--- src/main/java/com/learn/example/ExampleApplication.java
+++ src/main/java/com/learn/example/ExampleApplication.java
@@ -11,9 +11,9 @@ public class ExampleApplication {
 
 		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
 		System.out.println(myFirstService.tellAStory());
-		System.out.println(myFirstService.getJavaVersion());
-		System.out.println(myFirstService.getOsName());
-		System.out.println(myFirstService.readProperty());
+		System.out.println(myFirstService.getCustomPropertyInt());
+		System.out.println(myFirstService.getCustomProperty());
+		System.out.println(myFirstService.getCustomPropertyFromAnotherFile());
 
 	}
 
--- src/main/java/com/learn/example/MyFirstService.java
+++ src/main/java/com/learn/example/MyFirstService.java
@@ -2,45 +2,52 @@ package com.learn.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
+@PropertySources({
+        @PropertySource("classpath:custom.properties"),
+        @PropertySource("classpath:custom-file-2.properties")
+})
 public class MyFirstService {
 
 //    @Autowired
 //    @Qualifier("mySecondBean")
-    private  MyFirstClass myFirstClass;
-    private  Environment environment;
+    private  final MyFirstClass myFirstClass;
+
+    @Value("Hello am learning springboot")
+     private String customProperty;
 
 
-    @Autowired
-    public void setMyFirstClass(@Qualifier("bean1") MyFirstClass myFirstClass){
+    @Value("${my.prop}")
+    private String customPropertyFromAnotherFile;
+    @Value("123")
+    private  Integer customPropertyInt;
+
+
+    public MyFirstService(@Qualifier("bean1") MyFirstClass myFirstClass) {
         this.myFirstClass = myFirstClass;
     }
-
-
 
 
     public String tellAStory(){
       return  "the dependency is saying:"+ myFirstClass.sayHello();
     }
-    public String getJavaVersion(){
-        return environment.getProperty("java.version");
+
+    public String getCustomPropertyFromAnotherFile() {
+        return customPropertyFromAnotherFile;
+    }
+
+    public Integer getCustomPropertyInt() {
+        return customPropertyInt;
     }
 
 
-
-    @Autowired
-    public void setEnvironment(Environment environment) {
-        this.environment = environment;
-    }
-
-    public String readProperty(){
-        return environment.getProperty("my.custom.property");
-    }
-
-    public String getOsName(){
-        return environment.getProperty("os.name");
+    public String getCustomProperty() {
+        return customProperty;
     }
 }
