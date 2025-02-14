e9f6dc55bd6155f3cf47d1c70c29e0dadbc341fd
humphreyokoth <humpasp@gmail.com>
2025-02-14 13:39:46 +0300

    feat:profiles in springboot

--- src/main/java/com/learn/example/ApplicationConfig.java
+++ src/main/java/com/learn/example/ApplicationConfig.java
@@ -3,6 +3,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class ApplicationConfig {
@@ -10,11 +11,13 @@ public class ApplicationConfig {
 
 
     @Bean("bean1")
+    @Profile("dev")
     public MyFirstClass myFirstBean(){
         return new MyFirstClass("First Bean");
     }
 
     @Bean()
+    @Profile("test")
     public MyFirstClass mySecondBean(){
         return new MyFirstClass("Second Bean");
     }
--- src/main/java/com/learn/example/ExampleApplication.java
+++ src/main/java/com/learn/example/ExampleApplication.java
@@ -3,11 +3,15 @@ package com.learn.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class ExampleApplication {
 
 	public static void main(String[] args) {
-	  var ctx =	SpringApplication.run(ExampleApplication.class, args);
+		var app = new SpringApplication(ExampleApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active","test"));
+	     var ctx = app.run(args);
 
 		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
 		System.out.println(myFirstService.tellAStory());
