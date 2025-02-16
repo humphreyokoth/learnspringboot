38a0a8214ad4822ce728c40ee595a4b512c87280
humphreyokoth <humpasp@gmail.com>
2025-02-16 09:29:05 +0300

    feat:Rest

--- src/main/java/com/learn/example/ExampleApplication.java
+++ src/main/java/com/learn/example/ExampleApplication.java
@@ -9,16 +9,7 @@ import java.util.Collections;
 public class ExampleApplication {
 
 	public static void main(String[] args) {
-		var app = new SpringApplication(ExampleApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active","test"));
-	     var ctx = app.run(args);
-
-		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
-		System.out.println(myFirstService.tellAStory());
-		System.out.println(myFirstService.getCustomPropertyInt());
-		System.out.println(myFirstService.getCustomProperty());
-		System.out.println(myFirstService.getCustomPropertyFromAnotherFile());
-		System.out.println((myFirstService.getCustomPropertyFromAnotherFile2()));
+	     SpringApplication.run(ExampleApplication.class);
 
 	}
 
---
+++ src/main/java/com/learn/example/FirstController.java
@@ -0,0 +1,14 @@
+package com.learn.example;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello from my first controller";
+    }
+}
