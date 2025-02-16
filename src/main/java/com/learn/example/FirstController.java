a7a5dcb48e06e145e71e9f12ddc6f644663c694a
humphreyokoth <humpasp@gmail.com>
2025-02-16 09:39:14 +0300

    feat:Rest

--- src/main/java/com/learn/example/FirstController.java
+++ src/main/java/com/learn/example/FirstController.java
@@ -1,7 +1,10 @@
 package com.learn.example;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +13,12 @@ public class FirstController {
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello from my first controller";
+    }
+
+    @PostMapping("/post")
+    public String post(
+            String message
+    ){
+        return "Request accepted and message is :"+message;
     }
 }
