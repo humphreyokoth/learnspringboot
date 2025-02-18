0861d1ca060df246dcc4332834da6a45c8bc6452
humphreyokoth <humpasp@gmail.com>
2025-02-18 15:03:28 +0300

    feat:validation

--- src/main/java/com/learn/example/student/StudentController.java
+++ src/main/java/com/learn/example/student/StudentController.java
@@ -1,9 +1,14 @@
 package com.learn.example.student;
 
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -20,7 +25,7 @@ public class StudentController {
 
     @PostMapping("/students")
     public StudentResponseDto saveStudent(
-            @RequestBody StudentDto dto
+         @Valid @RequestBody StudentDto dto
     ){
           return  this.studentService.saveStudent(dto);
     }
@@ -54,6 +59,19 @@ public class StudentController {
     {
           this.studentService.delete(id);
 
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?>handleMethodArgumentNotException( MethodArgumentNotValidException exp)
+    {
+        var errors = new HashMap<String,String>();
+        exp.getBindingResult().getAllErrors()
+                .forEach(error->{
+                    var fieldName = ((FieldError)error).getField();
+                    var errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName,errorMessage);
+                });
+        return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
     }
 
 }
